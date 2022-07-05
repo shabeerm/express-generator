5f8d06d323f5b4a8610cf8db8b4a4e68f3a22693
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-07-05 17:11:32 +0400

    Assignment 2

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -5,53 +5,76 @@ const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
-leaderRouter.route('/')//mount this express router at the /dishes endpoint.chain all GET PUT POST DELET method already do this router
+const Leaders = require('../models/leaders');
 
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');//send plaintext replies back to client.
-        next();//looking for additional specifications down below here which will match /dishes endpoint
-    })
-
+leaderRouter.route('/')
     .get((req, res, next) => {
-        res.end('Will send all the leaders info to you!');
+        Leaders.find({})
+            .then((leaders) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
     .post((req, res, next) => {
-        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
-    })//parse body and added into the req object as req.body
+        Leaders.create(req.body)
+            .then((dish) => {
+                console.log('Leader Created ', dish);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
     .put((req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /leaders');
     })
     .delete((req, res, next) => {
-        res.end('Deleting all leaders info');
+        Leaders.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     });
 
 
 leaderRouter.route('/:leaderId')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();//looking for additional specifications down below here which will match /dishes endpoint
-    })
-
     .get((req, res, next) => {
-        res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+        Leaders.findById(req.params.leaderId)
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
-
     .post((req, res, next) => {
         res.statusCode = 403;
         res.end('POST operation not supported on /leaders/' + req.params.leaderId);
     })
-
     .put((req, res, next) => {
-        res.write('Updating the leader: ' + req.params.leaderId + '\n');
-        res.end('Will update the leader: ' + req.body.name +
-            ' with details: ' + req.body.description);
+        Leaders.findByIdAndUpdate(req.params.leaderId, {
+            $set: req.body
+        }, { new: true })
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
-
     .delete((req, res, next) => {
-        res.end('Deleting leader: ' + req.params.leaderId);
+        Leaders.findByIdAndRemove(req.params.leaderId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     });
 
 
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -5,53 +5,75 @@ const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
 
+const Promos = require('../models/promotions');
 
-promoRouter.route('/')//mount this express router at the /dishes endpoint.chain all GET PUT POST DELET method already do this router
-
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');//send plaintext replies back to client.
-        next();//looking for additional specifications down below here which will match /dishes endpoint
-    })
-
+promoRouter.route('/')
     .get((req, res, next) => {
-        res.end('Will send all the promotions to you!');
+        Promos.find({})
+            .then((promos) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promos);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
     .post((req, res, next) => {
-        res.end('Will add the promotions dish: ' + req.body.name + ' with details: ' + req.body.description);
-    })//parse body and added into the req object as req.body
+        Promos.create(req.body)
+            .then((dish) => {
+                console.log('Promotion Created ', dish);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
     .put((req, res, next) => {
         res.statusCode = 403;
-        res.end('PUT operation not supported on /promotions');
+        res.end('PUT operation not supported on /promos');
     })
     .delete((req, res, next) => {
-        res.end('Deleting all promotion info');
+        Promos.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     });
 
 promoRouter.route('/:promoId')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();//looking for additional specifications down below here which will match /dishes endpoint
-    })
-
     .get((req, res, next) => {
-        res.end('Will send details of the promotion dish: ' + req.params.promoId + ' to you!');
+        Promos.findById(req.params.promoId)
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
-
     .post((req, res, next) => {
         res.statusCode = 403;
-        res.end('POST operation not supported on /promotions/' + req.params.promoId);
+        res.end('POST operation not supported on /promos/' + req.params.promoId);
     })
-
     .put((req, res, next) => {
-        res.write('Updating the promotion dish: ' + req.params.promoId + '\n');
-        res.end('Will update the promotion dish: ' + req.body.name +
-            ' with details: ' + req.body.description);
+        Promos.findByIdAndUpdate(req.params.promoId, {
+            $set: req.body
+        }, { new: true })
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
-
     .delete((req, res, next) => {
-        res.end('Deleting promotion dish: ' + req.params.promoId);
+        Promos.findByIdAndRemove(req.params.promoId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     });
 
 
