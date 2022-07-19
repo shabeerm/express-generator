c09a2043f1a1d37f4bdacd5fed3eedf2b84880b1
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-07-19 12:28:31 +0400

    Authentication using passport-facebook

--- config.js
+++ config.js
@@ -4,5 +4,9 @@ const dbname = 'conFusion';
 
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': `mongodb+srv://conFusion:${DB_PASS}@confusion.lnvgz.mongodb.net/${dbname}?retryWrites=true&w=majority`
+    'mongoUrl': `mongodb+srv://conFusion:${DB_PASS}@confusion.lnvgz.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    'facebook': {
+        clientId: FB_clientId,
+        clientSecret: FB_clientSecret
+    }
 }
